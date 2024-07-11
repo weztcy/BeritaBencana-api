acf2efb5c7c7dd1e23a752ea5091b0361282a83f
Dimas Pratama <92010124+weztcy@users.noreply.github.com>
2024-07-11 16:32:14 +0700

    Update

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const cors = require('cors'); // Import middleware CORS
 const app = express();
 const articleRoutes = require('./src/routes/articleRoutes');
 
-app.use(express.json());
+// Gunakan middleware CORS
+app.use(cors());
 
-app.use( articleRoutes);
+app.use(express.json());
+app.use(articleRoutes);
 
 const PORT = process.env.PORT || 5000;
 
