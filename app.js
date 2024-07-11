624f6c7b6264e1ecd4a2fa07bf6ac039aaad4326
Dimas Pratama <92010124+weztcy@users.noreply.github.com>
2024-07-11 16:42:36 +0700

    Update

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 const express = require('express');
-const cors = require('cors'); // Import middleware CORS
 const app = express();
-const articleRoutes = require('./src/routes/articleRoutes');
-
-// Gunakan middleware CORS
-app.use(cors());
+const articleRoutes = require('./routes/articleRoutes');
 
 app.use(express.json());
-app.use(articleRoutes);
 
-const PORT = process.env.PORT || 5000;
+app.use('/api', articleRoutes);
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
